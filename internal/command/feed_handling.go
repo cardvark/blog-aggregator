5caba258a033da7423d52c8cc6ab71ee0e24c116
James Wang <wang.james.j@gmail.com>
2026-01-22 10:58:04 -0800

    Moved feed follow handling to its own file.

--- internal/command/feed_handling.go
+++ internal/command/feed_handling.go
@@ -12,68 +12,6 @@ import (
 	"github.com/google/uuid"
 )
 
-func handlerFollow(s *state, cmd command) error {
-	if len(cmd.args) < 1 {
-		fmt.Println("Error: URL required.")
-		os.Exit(1)
-		return fmt.Errorf("Error: no arguments found for %s:\n", cmd.name)
-	}
-
-	feedURL := cmd.args[0]
-
-	feed, err := s.db.GetFeedByURL(
-		context.Background(),
-		feedURL,
-	)
-	if err != nil {
-		fmt.Printf("Error retrieving feed by url: %v\n", err)
-		return err
-	}
-
-	userName := s.config.Current_user_name
-
-	user, err := s.db.GetUser(
-		context.Background(),
-		userName,
-	)
-	if err != nil {
-		fmt.Println("Current user not found in database.")
-		os.Exit(1)
-		return err
-	}
-
-	user_id := user.ID
-	feed_id := feed.ID
-
-	now := time.Now()
-	var nullableTime sql.NullTime
-	nullableTime.Time = now
-	nullableTime.Valid = true
-
-	feedFollowParams := database.CreateFeedFollowParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now(),
-		UpdatedAt: nullableTime,
-		UserID:    user_id,
-		FeedID:    feed_id,
-	}
-
-	feedFollow, err := s.db.CreateFeedFollow(
-		context.Background(),
-		feedFollowParams,
-	)
-	if err != nil {
-		fmt.Printf("Error inserting feedfollow: %v\n", err)
-		return err
-	}
-
-	fmt.Printf("%v\n", feedFollow)
-	fmt.Printf("%s is now following feed: %s.\n", userName, feedFollow.FeedName)
-
-	return nil
-
-}
-
 func handlerAddFeed(s *state, cmd command) error {
 	if len(cmd.args) < 2 {
 		fmt.Println("Error: feed (1) name and (2) URL required.")
